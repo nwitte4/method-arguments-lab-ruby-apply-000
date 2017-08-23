99bd5099ca5af16f7b130b747d32ca69ac8293ea
Nicole Witte <nwitte@haverford.edu>
2017-08-23 20:47:16 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction(name=Nicole)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(name=Nicole, language=Ruby)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
